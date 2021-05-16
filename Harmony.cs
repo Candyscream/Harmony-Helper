4bbeb842366386af6817110cc06fa3bdf9258f1f
Candyscream <Candyscream@outlook.com>
2021-05-16 14:05:14 +0200

    Add project files.

---
+++ Harmony.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmony_Helper
+{
+    public static class MusicalScale
+    {
+        public static int[] Aeolian = new int[] { 1, 2, 1, 1, 2, 1, 1 };
+        public static int[] Locrian = new int[] { 2, 1, 1, 2, 1, 1, 1 };
+        public static int[] Ionian = new int[] { 1, 1, 2, 1, 1, 2, 1 };
+        public static int[] Dorian = new int[] { 1, 2, 1, 1, 1, 2, 1 };
+        public static int[] Phrygian = new int[] { 1, 2, 1, 1, 1, 2, 1 };
+        public static int[] Lydian = new int[] { 1, 2, 1, 1, 1, 2, 1 };
+        public static int[] Mixolydian = new int[] { 1, 2, 1, 1, 1, 2, 1 };
+        public static int[] Minor = Aeolian;
+        public static int[] Major = Ionian;
+
+        public static int[] ToValue(int[] scale)
+        {
+            int sum = 0;
+            int[] values = new int[7];
+
+            for (int ii = 0; ii < 7; ++ii)
+            {
+                values[ii] = sum;
+                sum += scale[ii];
+            }
+            return values;
+        }
+    }
+
+    class Node
+    {
+        Harmony.Note m_note = Harmony.Note.C;
+        bool m_isMinor = false;
+        int m_offset = 0;
+        string m_chordSymbols = "maj7";
+
+        public Node(Harmony.Note note, int offset, bool isMinor)
+        {
+            m_isMinor = isMinor;
+            m_offset = offset;
+            m_note = note;
+        }
+        public string BaseNote
+        {
+            get
+            {
+                return m_note + Harmony.signs[m_offset];
+            }
+        }
+        public string Chord
+        {
+            get
+            {
+                string chordName = m_isMinor ? BaseNote.ToLower() : BaseNote;
+                chordName += m_chordSymbols;
+                return chordName;
+            }
+        }
+
+        public string Notes
+        {
+            // TODO
+            get { return BaseNote; }
+        }
+    }
+
+    class Harmony
+    {
+        public static readonly Dictionary<int, string> signs = new Dictionary<int, string>()
+        {
+            { -2, "bb" },
+            { -1, "b" },
+            { 0, "" },
+            { 1, "#" },
+            { 2, "##" }
+        };
+
+        public enum Note
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G
+        };
+
+        Note GetNote(int index)
+        {
+            return (Note)(index % 7);
+        }
+
+        public ObservableCollection<Node> Nodes = new ObservableCollection<Node>();
+        public ObservableCollection<string> Scale = new ObservableCollection<string>();
+        private int[] baseScale;
+
+
+        public Harmony(Note mean = Note.C, bool minor = false)
+        {
+            baseScale = minor ? MusicalScale.Minor : MusicalScale.Major;
+            foreach (int ii in MusicalScale.ToValue(baseScale))
+            {
+                var note = GetNote((int)mean + ii);
+                Nodes.Add(new Node(note, 0, minor));
+                Scale.Add("" + ii);
+            }
+        }
+
+
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Harmony_Helper;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Harmony_Helper
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Harmony reference = new Harmony(Harmony.Note.C);
+        Harmony mine = new Harmony(Harmony.Note.A);
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
